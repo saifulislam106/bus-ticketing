2bbc1486bf77b25e6d72d666a1752a629154cf04
saifulislam106 <saifulislam106915@gmail.com>
2024-02-22 22:49:23 +0600

    final commited

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 
 
-const seatNum = document.getElementsByClassName('seat-num');
-console.log(seatNum);
+let selectedSeats = [];
+let totalPrice = 0;
 
-for(const seat of seatNum){
-    seat.addEventListener('click', function(event){
-        console.log(event.target);
-    })
+function selectSeat(seatId){
+    const seatIndex = selectedSeats.indexOf(seatId);
+    if (seatIndex === -1){
+        selectedSeats.push(seatId);
+        totalPrice += 550;
+    }
+    else{
+        selectedSeats.splice(seatIndex , 1);
+        totalPrice -= 550;
+        updateSeatInfo();
+    }
+}
+
+function updateSeatInfo(){
+    const seatInfoElement = document.getElementById('seatInfo');
+    const totalPriceElement = document.getElementById('totalPrice');
+
+    seatInfoElement.innerText = '';
+    selectedSeats.forEach(seatId =>{
+        const seatInfo = document.createElement('div');
+        
+        seatInfo.textContent = ` ${seatId}, economy, 550 `;
+        seatInfo.classList.add('flex',);
+        seatInfo.classList.add('justify-around');
+    });
+    totalPriceElement.textContent = totalPrice
 }
